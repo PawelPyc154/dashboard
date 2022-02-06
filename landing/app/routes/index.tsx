19acec6df36cfd9232f338ebcfced3a4b7b18e90
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2022-02-06 19:54:22 +0100

    fix

--- landing/app/routes/index.tsx
+++ landing/app/routes/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <Link to="/app">dashboard</Link>
-      <a href="https://pawelpyc.pl/app">dashboard</a>
+      <a href="https://pawelpyc.pl/app">dashboard</a> test
     </div>
   );
 }
