be505957b3770508c0090a87a5dbaf12e16f9d5e
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2022-02-06 20:50:28 +0100

    fix routing

--- landing/app/routes/index.tsx
+++ landing/app/routes/index.tsx
@@ -3,8 +3,7 @@ import { Link } from "remix";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <Link to="/app">dashboard</Link>
-      <a href="https://pawelpyc.pl/app">dashboard</a> test
+      <a href="https://pawelpyc.pl/app">dashboard</a>
     </div>
   );
 }
