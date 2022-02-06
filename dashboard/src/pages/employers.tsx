6473cbc876429e5ee756f249047f5a6e0a530e09
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2022-02-06 21:05:41 +0100

    try localstorage

--- dashboard/src/pages/employers.tsx
+++ dashboard/src/pages/employers.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { DialogBase } from '../components/common/dialog/dialogBase'
 import { Menu, MenuItem } from '../components/common/menu'
 import { Button } from '../components/form/button'
@@ -7,6 +8,7 @@ import 'styled-components/macro'
 
 const Employers = () => {
   const { isOpenDialog, handleOpenDialog, handleCloseDialog } = useDialog()
+  const [inputValue, setInputValue] = useState('')
   return (
     <div tw="flex">
       <Button onClick={handleOpenDialog}>open Dialog </Button>
@@ -16,8 +18,9 @@ const Employers = () => {
         </DialogBase>
       )}
       <a href="/">test</a>
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
       <a href="https://pawelpyc.pl">test</a>
-
+      {inputValue}
       <div tw="font-bold">{localStorage.getItem('test')}</div>
       <Menu openButton={<Button>open Dialog </Button>}>{() => <MenuItem>test</MenuItem>}</Menu>
     </div>
