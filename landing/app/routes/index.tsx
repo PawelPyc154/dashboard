e3c23bfa83502a69fb6a5e98503c530367a59fb6
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2022-02-06 18:37:20 +0100

    fix deploy

--- landing/app/routes/index.tsx
+++ landing/app/routes/index.tsx
@@ -1,17 +1,31 @@
-import { Link } from "remix";
-
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
       <ul>
-        <Link to="app" className="button">
-          app
-        </Link>
-        <div>
-          <a href="https://pawelpyc.pl/app" className="button">
-            a app
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
           </a>
-        </div>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
       </ul>
     </div>
   );
